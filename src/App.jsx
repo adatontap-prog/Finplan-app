f66ca0621ca576b834b0b7f3d4f80087285ee3b4
adatontap <adatontap@gmail.com>
2026-06-28 11:06:58 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2201,4 +2201,4 @@ export default function App() {
       <style>{`* { margin:0; padding:0; box-sizing:border-box; } ::-webkit-scrollbar { display:none; }`}</style>
     </div>
   );
-}
+                                                                                   }
